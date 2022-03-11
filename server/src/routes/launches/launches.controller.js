70f718be4285c60f7c123c6525bc636e75f9a95e
skyrippa <iagob4599@gmail.com>
2022-03-11 17:12:09 -0300

    add data access layer

--- server/src/routes/launches/launches.controller.js
+++ server/src/routes/launches/launches.controller.js
@@ -1,10 +1,9 @@
-const { launchesModel } = require('../../models/launches.model')
+const { getAllLaunches } = require('../../models/launches.model')
 
-function getAllLaunches(req, res) {
-    console.log(launchesModel)
-    return res.status(200).json(Array.from(launchesModel.values()))
+function httpGetAllLaunches(req, res) {
+    return res.status(200).json(getAllLaunches())
 }
 
 module.exports = {
-    getAllLaunches,
+    httpGetAllLaunches,
 }
